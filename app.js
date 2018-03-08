9bf77a2593bd6eff764c6f1bf3810b62f3279731
ElliahSon <30949066+ElliahSon@users.noreply.github.com>
2018-03-07 14:17:07 -1000

    owari

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var arr =["item1"; "item2"; "item3"; "item4"];
+var arr =["item1", "item2", "item3", "item4"];
 
 var num = 3;
 
+function loop() {
+  for (i=0, i<arr.length, i++)
+    document.appendChild("fourth") = loop();
+}
 
+var object = {
+  first: Elliah;
+  last: Herradura;
+  arr2: [
+        "bacon",
+        "hamburger",
+        "potato"];
+};
 
+document.getElementById("fourth")inner.html = object.first;
+document.getElementById("fourth")inner.html = object.last;
+document.getElementById("fourth")inner.html = object.arr2;
 
 
 
